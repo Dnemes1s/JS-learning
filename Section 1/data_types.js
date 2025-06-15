eff59d781946d8cf266262e4c38fa0606361e52f
Brandon <twistshadow9@gmail.com>
2025-06-16 08:17:11 +1000

    Section 1
    JS stuff

---
+++ Section 1/data_types.js
@@ -0,0 +1,16 @@
+let age = 23; // Number type dat
+
+let fName = "Brandon"; //  String data type
+
+let fullAge = true; // Boolean data type. Only ever true of false
+
+let children; // Undefined data type. Empty value
+            // Null is also an empty value data type
+
+// Symbol data type isnt useful or used much anymore
+
+// Big int data type is a number that is too large to be represented by the number data type
+
+age = 55 // This is how you change the value in a variable 
+
+
